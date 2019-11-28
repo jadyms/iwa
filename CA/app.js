fe1f80ef0d5c01fdb83f7b884f337d6088c67de4
Jady Martins Silva <jady.ms@outlook.com>
2019-11-28 14:26:22 +0000

    7.JavaScript

--- CA/app.js
+++ CA/app.js
@@ -2,6 +2,7 @@ var http = require('http'),
     path = require('path'),
     express = require('express'),
     fs = require('fs'),
+    xml2js = require('xml2js'),
     xmlParse = require('xslt-processor').xmlParse,
     xsltProcess = require('xslt-processor').xsltProcess;
 
@@ -9,6 +10,39 @@ var router = express();
 var server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname, 'views')));
+
+
+router.use(express.urlencoded({extended: true}));
+router.use(express.json());// Function to read in XML file and convert it to JSON
+function xmlFileToJs(filename, cb) {
+  var filepath = path.normalize(path.join(__dirname, filename));
+  fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+    if (err) throw (err);
+    xml2js.parseString(xmlStr, {}, cb);
+  });
+}//Function to convert JSON to XML and save it
+function jsToXmlFile(filename, obj, cb) {
+  var filepath = path.normalize(path.join(__dirname, filename));
+  var builder = new xml2js.Builder();
+  var xml = builder.buildObject(obj);
+  fs.writeFile(filepath, xml, cb);
+}
+router.post('/post/json', function(req, res){
+    function appendJSON(obj){
+        console.log(obj);
+        xmlFileToJs('PaddysCafe.xml', function(err, result){
+            if(err) throw (err);
+                result.cafemenu.section[obj.sec_n].entree.push({'item': obj.item, 'price': obj.price});
+                console.log(result);
+                jsToXmlFile('PaddysCafe.xml', result, function(err){
+                    if(err) console.log(err);
+                })
+        })
+    }
+    appendJSON(req.body);
+    res.redirect('back');
+});
+
 
 router.get('/', function(req, res){
 
